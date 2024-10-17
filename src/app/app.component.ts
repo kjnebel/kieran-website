63a0f91d2a292151434aa96f0b05c85d5051c594
kjnebel <nebelkj.13@gmail.com>
2024-10-17 05:20:27 -0500

    Added a navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'kieran-website';
+
+  ngOnInit() {
+    try {
+      window.addEventListener('scroll', function() {
+        let nav = document.getElementById("nav");
+        if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+          nav!.style.background = "var(--background-grey)";
+          nav!.style.boxShadow = "var(--box-shadow)";
+        } else {
+          nav!.style.background = "var(--top-gradient)";
+          nav!.style.boxShadow = "none";
+        }
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
 }
